35d40f5944eaeb3b593b1263a6836c283486ef94
Oleh Kurachenko <oleh.kurachenko@gmail.com>
2018-05-09 23:52:51 +0300

    definition moved from class body down, minor changes in const qualifiers

--- src/Point.cpp
+++ src/Point.cpp
@@ -1,24 +1,12 @@
 //
-// Created by soll_nevermind on 1/29/18.
+//  Created by Oleh Kurachenko
+//         aka okurache
+//
+//  ask me      oleh.kurachenko@gmail.com , I'm ready to help
+//  GitHub      https://github.com/OlehKurachenko
+//  rate & CV   http://www.linkedin.com/in/oleh-kurachenko-6b025b111
 //
 
 #include "Point.h"
 
 unsigned int Point::freeID = 0;
-
-std::ostream& operator<<(std::ostream &ostr, const Point &point) {
-	ostr << "Point{x=" << point.x() << ",y=" << point.y() << ",ID=" << point.getID() << '}';
-	return ostr;
-}
-
-Point& operator+=(Point &one, const Point &another) {
-	one.x() += another.x();
-	one.y() += another.y();
-	return one;
-}
-
-Point& operator-=(Point &one, const Point &another) {
-	one.x() += another.x();
-	one.y() += another.y();
-	return one;
-}
